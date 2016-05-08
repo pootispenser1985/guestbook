5085101b4ebdcf85c8e48fe3921c5968ecc47ca4
jason <jason@pootis>
2016-05-08 16:34:07 -0500

    php speaks to sql

--- main.php
+++ main.php
@@ -4,8 +4,21 @@
 <body>
 <?php
   include '/home/thrashca/etc/guestbook.php';
+  $db = new mysqli('localhost', $sqlUser, $sqlPass, 'thrashca_guestbook');
 
-  echo 'success';
+  $query = "SELECT * FROM posts;";
+  $result = $db->query($query);
+
+  echo "<p>";
+  while (2 < 5) {
+    $line = $result->fetch_array();
+    if ($line == NULL) {break;}
+    for ($i=0; $i<4; $i++) {
+      echo $line[$i]." ";
+    }
+    echo "</p><p>";
+  }
+  echo "</p>"
 ?>
 
 </body>
