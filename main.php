cbb8a768ea17a32b04444b804adcf4b27b056a57
pootispenser1985 <jigostowski@gmail.com>
2016-05-10 23:05:51 -0500

    reversed order of comments, and page

--- main.php
+++ main.php
@@ -4,12 +4,21 @@
   <link rel="stylesheet" type="text/css" href="guestbook.css?v=2002" />
 </head>
 <body>
+
+<div id="newComment">
+  <p>Add a comment!</p>
+  <form action="inserter.php" method="post">
+    <textarea name="comment" rows="5" cols="50"></textarea>
+    <input type="submit" value="submit">
+  </form>
+</div>
+
 <div id="comments">
   <?php
     include '/home/thrashca/etc/guestbook.php';
     $db = new mysqli('localhost', $sqlUser, $sqlPass, 'thrashca_guestbook');
 
-    $query = "SELECT * FROM posts;";
+    $query = "SELECT * FROM posts ORDER BY id DESC;";
     $result = $db->query($query);
 
     echo "<p>";
@@ -25,11 +34,6 @@
   ?>
 </div>
 
-<p>Add a comment!</p>
-<form action="inserter.php" method="post">
-  <textarea name="comment" rows="5" cols="50"></textarea>
-  <input type="submit" value="submit">
-</form>
 
 </body>
 </html>
